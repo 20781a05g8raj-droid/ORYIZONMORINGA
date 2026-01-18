c89175b2601e097109a3a03965dac3af9d21f67d
20781a05g8raj-droid <rajenshu8905@gmail.com>
2026-01-19 01:22:06 +0545

    Remove Gallery3D section as requested

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,12 @@ import Hero from "@/components/Hero";
 import ProductShowcase from "@/components/ProductShowcase";
 import FeaturedProducts from "@/components/FeaturedProducts";
 import BlogGrid from "@/components/BlogGrid";
-import Gallery3D from "@/components/Gallery3D";
 import { ScrollReveal } from "@/components/ui/ScrollReveal";
 
 export default function Home() {
   return (
     <main className="bg-background overflow-hidden">
       <Hero />
-
-      <Gallery3D />
 
       <ScrollReveal width="100%" direction="up">
         <ProductShowcase />
